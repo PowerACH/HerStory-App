8e4df8133a7ee4860aa181a01cbf11f4a635d433
Rachel Powe <rachelpowe@gmail.com>
2020-07-30 00:38:04 -0400

    updates

--- src/Component/Layout/NavBar.js
+++ src/Component/Layout/NavBar.js
@@ -6,11 +6,11 @@ function NavBar() {
 return(
 
 <Navbar className="Navbar" bg="light" variant="light">
-    <Navbar.Brand href="/Main"> INSTA IMG </Navbar.Brand>
+    <Navbar.Brand href="/"> INSTA IMG </Navbar.Brand>
     <Nav className="navbar-nav ml-auto">
-      <Nav.Link to= "/Main">Home</Nav.Link>
+      <Nav.Link href= "/">Home</Nav.Link>
       <Nav.Link href="/UserProfile">Profile</Nav.Link>
-      <Nav.Link href="/Post">Post</Nav.Link>
+      <Nav.Link href="/Upload">Post</Nav.Link>
       <Nav.Link href="#favorites">Favorites</Nav.Link>
       <Nav.Link href="/SignedOut">Log Out</Nav.Link>
       {/* <Navbar.Collapse className="align-self-end"> */}
