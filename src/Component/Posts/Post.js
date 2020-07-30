8572f03560149b33232331daf424a30236268f31
Rachel Powe <rachelpowe@gmail.com>
2020-07-30 01:12:11 -0400

    updates

--- src/Component/Posts/Post.js
+++ src/Component/Posts/Post.js
@@ -1,38 +1,23 @@
 import React from "react";
 import './Post.css';
-import PostData from './data.json'
+import PostFormat from './PostFormat'
 
     
 // export default class Post extends React.Component {
-const Post = (posts) => {
+const Post = ({posts}) => {
 
     // render() {
         return (
             <div>
-        {posts && posts.map((post, i) => {
-            return 
-            <article className = "photoContainer" key={i}>
-                <header>
-                    <div className = "postUser">
-                        <div className = "postProfileImg">
-                            <img src={details.userImage} alt="profile" />
-                        </div>
-                        <div className = "postUserName">
-                            {details.username}
-                        </div>
-                    </div>
-                </header>
-                <div className = "imagePost">
-                    <div className = "imagePostImage">
-                        <img src={details.postImage} alt="Post"/>
-                    </div>
-                </div>
-                <div className = "caption">
-                    <strong>{details.username}</strong>  {details.imageCaption}
-                </div>
-            </article>
-        })}
+                {
+                    posts.map(post => {
+                    return (
+                    <PostFormat post={post} key={post.id} />
+                    )
+                })}
             </div>
         )
     // }
 }
+
+export default Post;
--- src/Component/Posts/PostFormat.js
+++ src/Component/Posts/PostFormat.js
@@ -1,5 +1,28 @@
 import React from 'react';
 
-const PostFormat = () => {
-    
+const PostFormat = (post) => {
+    return (
+        <article className = "photoContainer">
+                <header>
+                    <div className = "postUser">
+                        <div className = "postProfileImg">
+                            <img src={post.userImage} alt="profile" />
+                        </div>
+                        <div className = "postUserName">
+                            {post.username}
+                        </div>
+                    </div>
+                </header>
+                <div className = "imagePost">
+                    <div className = "imagePostImage">
+                        <img src={post.postImage} alt="Post"/>
+                    </div>
+                </div>
+                <div className = "caption">
+                    <strong>{post.username}</strong>  {post.imageCaption}
+                </div>
+            </article>
+    )
 }
+
+export default PostFormat;
