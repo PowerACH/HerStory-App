0477e9596e139e8de788012441aa808c733ebc46
Rachel Powe <rachelpowe@gmail.com>
2020-08-20 18:52:17 -0400

    added middleware

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './store/reducers/rootReducer'
 import { Provider } from 'react-redux' //binder of react and redux
+import thunk from 'redux-thunk';
 
 //create store inside store variable
 //pass root reducer inside 'createStore' reducer
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 // import Provider so App has access to the store
 ReactDOM.render(
