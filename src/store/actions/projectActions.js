ddccbefdab2421d1b3eab61efa5d4bf928a197fe
Rachel Powe <rachelpowe@gmail.com>
2020-08-20 19:35:02 -0400

    renamed

--- src/store/actions/projectActions.js
+++
@@ -1,7 +0,0 @@
-export const createProject = (project) => {
-    return (dispatch, getState) => {
-        //Pause dispatch
-        //Make an async call to database
-        dispatch({ type: 'CREATE_PROJECT' , project });
-    }
-}
